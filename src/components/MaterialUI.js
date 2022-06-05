69a7fbc677866fbd6dbfc7b3f1f7726f51a0f900
Gabriel <ggauto994@gmail.com>
2022-06-05 15:53:36 -0400

    CLASE 25 Y 26 DE FRAMEWORKS VIA CLASICA O COMPONENTES

---
+++ src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MaterialCard from "./MaterialCard";
+import PersistentDrawer from "./PersistentDrawer";
+
+export default function MaterialUI() {
+  return (
+    <>
+      <h2>CLASE 26 - MATERIAL UI</h2>
+      <MaterialCard />
+      <PersistentDrawer />
+    </>
+  );
+}
